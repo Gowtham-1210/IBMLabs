000bc050d2ef7fdc51ea1dcca46eb25049705b72
Gowtham-1210 <54500445+Gowtham-1210@users.noreply.github.com>
2020-02-25 10:17:54 +0530

    Add files via upload

---
+++ Literals.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "orange", "mango"]
+numbers = (1,2,3,4,5)
+alphabets = {'a':'automation','b':'bot','c':'chat'}
+vowels = {'a','e','i','o','u'}
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
